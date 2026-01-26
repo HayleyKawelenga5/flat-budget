0458a5456d725f17857e1dd29891329282ea196f
Hayley Kawelenga <hka105@uclive.ac.nz>
2026-01-26 19:57:03 +1300

    Set up CDK stack with AppSync and DynamoDB

---
+++ infra/lib/infra-stack.ts
@@ -0,0 +1,29 @@
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as appsync from 'aws-cdk-lib/aws-appsync';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+
+export class InfraStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const api = new appsync.GraphqlApi(this, 'FlatBudgetApi', {
+      name: 'flat-budget-api',
+      schema: appsync.SchemaFile.fromAsset(
+        '../backend/schema/schema.graphql'
+      ),
+      authorizationConfig: {
+        defaultAuthorization: {
+          authorizationType: appsync.AuthorizationType.API_KEY,
+        },
+      },
+      xrayEnabled: true,
+    });
+
+    new dynamodb.Table(this, 'FlatBudgetTable', {
+      partitionKey: { name: 'PK', type: dynamodb.AttributeType.STRING },
+      sortKey: { name: 'SK', type: dynamodb.AttributeType.STRING },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+    });
+  }
+}
