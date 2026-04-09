3714fc9dbec2307edfb01345c6411f20e84cc394
Hayley Kawelenga <hka105@uclive.ac.nz>
2026-04-09 13:24:20 +1200

    define lambdas and resolvers

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -21,26 +21,77 @@ export class InfraStack extends Stack {
       xrayEnabled: true,
     });
 
-    new dynamodb.Table(this, 'FlatBudgetTable', {
+    const table = new dynamodb.Table(this, 'FlatBudgetTable', {
       partitionKey: { name: 'PK', type: dynamodb.AttributeType.STRING },
       sortKey: { name: 'SK', type: dynamodb.AttributeType.STRING },
       billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
     });
 
+    // Lambda: Get Budget Summary (monthlySummary query)
     const getBudgetSummaryFn = new lambda.Function(this, 'GetBudgetSummaryFn', {
-    runtime: lambda.Runtime.NODEJS_18_X,
-    handler: 'index.handler',
-    code: lambda.Code.fromAsset('lambda/getBudgetSummary'),
+      runtime: lambda.Runtime.NODEJS_18_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset('lambda/getBudgetSummary'),
+      environment: {
+        TABLE_NAME: table.tableName,
+      },
     });
 
-    const lambdaDataSource = api.addLambdaDataSource(
-    'BudgetLambdaDS',
-    getBudgetSummaryFn
+    // Lambda: Add Expense (addExpense mutation)
+    const addExpenseFn = new lambda.Function(this, 'AddExpenseFn', {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset('lambda/addExpense'),
+      environment: {
+        TABLE_NAME: table.tableName,
+      },
+    });
+
+    // Lambda: Get Expenses (expenses query)
+    const getExpensesFn = new lambda.Function(this, 'GetExpensesFn', {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset('lambda/getExpenses'),
+      environment: {
+        TABLE_NAME: table.tableName,
+      },
+    });
+
+    // Data Sources
+    const summaryDataSource = api.addLambdaDataSource(
+      'BudgetSummaryDS',
+      getBudgetSummaryFn
     );
 
-    lambdaDataSource.createResolver('MonthlySummaryResolver', {
-    typeName: 'Query',
-    fieldName: 'monthlySummary',
+    const addExpenseDataSource = api.addLambdaDataSource(
+      'AddExpenseDS',
+      addExpenseFn
+    );
+
+    const getExpensesDataSource = api.addLambdaDataSource(
+      'GetExpensesDS',
+      getExpensesFn
+    );
+
+    // Resolvers
+    summaryDataSource.createResolver('MonthlySummaryResolver', {
+      typeName: 'Query',
+      fieldName: 'monthlySummary',
     });
+
+    addExpenseDataSource.createResolver('AddExpenseResolver', {
+      typeName: 'Mutation',
+      fieldName: 'addExpense',
+    });
+
+    getExpensesDataSource.createResolver('GetExpensesResolver', {
+      typeName: 'Query',
+      fieldName: 'expenses',
+    });
+
+    // Permissions
+    table.grantReadData(getBudgetSummaryFn);
+    table.grantReadWriteData(addExpenseFn);
+    table.grantReadData(getExpensesFn);
   }
 }
