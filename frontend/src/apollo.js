e53f6e0aad259c0db83ae1c77a61b3a0609088e5
Hayley Kawelenga <hka105@uclive.ac.nz>
2026-04-09 15:52:08 +1200

    changed env variables to use vite

--- frontend/src/apollo.js
+++ frontend/src/apollo.js
@@ -2,9 +2,9 @@ import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
 export const client = new ApolloClient({
   link: new HttpLink({
-    uri: process.env.REACT_APP_GRAPHQL_ENDPOINT,
+    uri: import.meta.env.VITE_GRAPHQL_ENDPOINT,
     headers: {
-      'x-api-key': process.env.REACT_APP_API_KEY,
+      'x-api-key': import.meta.env.VITE_API_KEY,
     },
   }),
   cache: new InMemoryCache(),
